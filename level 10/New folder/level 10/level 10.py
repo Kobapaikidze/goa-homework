5c106954a511da307e378bcde3c8795609e7d582
kobapaikidze <paikidzekoba9@gmail.com>
2024-06-22 14:27:49 +0400

    commit

---
+++ level 10/New folder/level 10/level 10.py
@@ -0,0 +1,14 @@
+print(10 < 15) # True
+print(14 > 50) # False
+print(13>=13) # True
+print(15<=14)# False
+print(17==18) # false
+print(11 != 11) #false
+print(15 == 15 or 15 < 17 ) # True
+print(15 < 17 and 18 > 14) # True 
+print(True or False)
+print(True and False)
+print(True or True)
+print(False or False)
+print(True and True )
+print( False and False) 
